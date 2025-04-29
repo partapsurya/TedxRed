e11d73cc3ef7b33771b90b889905192d5c8189de
partapsurya <partapsurya2005@gmail.com>
2025-04-29 23:53:57 +0530

    about fix

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const data = {
     ted: {
         title: "TED",
         description: "TED is a nonprofit, nonpartisan organization dedicated to discovering, debating and spreading ideas that spark conversation, deepen understanding and drive meaningful change. Our organization is devoted to curiosity, reason, wonder and the pursuit of knowledge — without an agenda. We welcome people from every discipline and culture who seek a deeper understanding of the world and connection with others, and we invite everyone to engage with ideas and activate them in your community.",
-        img: "homepage/4.jpg"  // Update with the correct path
+        img: "Homepage/4.jpg"  // Update with the correct path
     },
     tedx: {
         title: "TEDx",
         description: "In the spirit of discovering and spreading ideas, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TED Talks video and live speakers combine to spark deep discussion and connection. These local, self-organized events are branded TEDx, where x = independently organized TED event. The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized. (Subject to certain rules and regulations.)",
-        img: "homepage/5.jpg"  // Update with the correct path
+        img: "Homepage/5.jpg"  // Update with the correct path
     },
     tedxtiet: {
         title: "TEDxTIET",
         description: "TEDxTIET is an annual independently organized TED event that commenced in 2015. It is driven by a dedicated team of passionate volunteers, comprising undergraduate and post-graduate students of Thapar Institute of Engineering and Technology (TIET). The main conference at TEDxTIET is a culmination of months of meticulous preparation, bringing together visionaries, innovators, and changemakers for an extraordinary day of exploration and inspiration. Set against the backdrop of Thapar University in Patiala, this event transcends traditional conferences, embodying a celebration of human creativity and resilience.",
-        img: "homepage/7.jpg"  // Update with the correct path
+        img: "Homepage/7.jpg"  // Update with the correct path
     }
 };
   
